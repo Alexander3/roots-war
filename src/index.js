d661a3ca7041308efecfa8209773ac7985afa28e
Aleksander <kawalaaleksander@gmail.com>
2023-02-04 00:10:53 +0100

    add game loop

--- src/index.js
+++ src/index.js
@@ -1,35 +1,42 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import Phaser from "phaser";
+import logoImg from "./assets/logo.png";
 
-class MyGame extends Phaser.Scene {
-    constructor() {
-        super();
-    }
+class MyScene extends Phaser.Scene {
+  constructor() {
+    super();
+  }
 
-    preload() {
-        this.load.image('logo', logoImg);
-    }
+  preload() {
+    this.load.image("logo", logoImg);
+  }
 
-    create() {
-        const logo = this.add.image(400, 150, 'logo');
+  create() {
+    const logo = this.add.image(400, 150, "logo");
 
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1,
+    });
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: MyScene,
 };
 
-const game = new Phaser.Game(config);
+class MyGame extends Phaser.Game {
+  step(time, delta) {
+    super.step(time, delta);
+    console.log("dupa");
+  }
+}
+
+const game = new MyGame(config);
